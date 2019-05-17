2ef2e56927da1356e7f8379b7e319e7049ef812f
rendrop <gusvananda@gmail.com>
2019-05-17 16:09:42 +0700

    Update to SIMAK update May 2019
    Minor changes in

--- epbm_bot.js
+++ epbm_bot.js
@@ -3,19 +3,19 @@ function hackEPBM(jumlah_dosen, kesan){
 	ma = kesan + 1
 	for(i=1;i<=10;i++){
 		choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
-		formattedNumber = ("8" + i).slice(-2)
+		formattedNumber = ("6" + i).slice(-2)
 		if(i==10){
-			formattedNumber = ("90")
+			formattedNumber = ("70")
 		}
-		target = "#JawabanMK_14"+formattedNumber+choose
+		target = "#JawabanMK_15"+formattedNumber+choose
 		$(target).click();
 	}
 	// isi form dosen
 	for(i=1;i<=jumlah_dosen;i++){
 		for(j=1;j<=10;j++){
-			formattedNumber = ("155" + j)
+			formattedNumber = ("163" + j)
 		if(j==10){
-			formattedNumber = ("1560")
+			formattedNumber = ("1640")
 }
 			choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
 			target = "#JawabanDosen_"+i+j+formattedNumber+choose
@@ -24,10 +24,3 @@ function hackEPBM(jumlah_dosen, kesan){
 		}
 	}
 }
-
-/*kesan:
-1 Sangat Tidak Setuju atau Tidak Setuju
-2 Tidak Setuju atau Setuju
-3 Setuju atau Sangat Setuju
-*/
-//hackEPBM(jumlah_dosen,kesan)
