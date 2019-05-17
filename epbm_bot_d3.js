c819d953c95ba076db6d0fd9bc585dc9e799caa1
rendrop <gusvananda@gmail.com>
2019-05-17 16:29:06 +0700

    EPBM Bot Script for Vocational Student

---
+++ epbm_bot_d3.js
@@ -0,0 +1,28 @@
+function hackEPBM(jumlah_dosen, kesan){
+	mi = kesan
+	ma = kesan + 1
+	for(i=1;i<=10;i++){
+		choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
+		formattedNumber = ("7" + i).slice(-2)
+		if(i==10){
+			formattedNumber = ("80")
+		}
+		target = "#JawabanMK_15"+formattedNumber+choose
+		$(target).click();
+	}
+	// isi form dosen
+	for(i=1;i<=jumlah_dosen;i++){
+		for(j=1;j<=10;j++){
+			formattedNumber = ("162" + j)
+		if(j==10){
+			formattedNumber = ("1630")
+}
+			choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
+			target = "#JawabanDosen_"+i+j+formattedNumber+choose
+			$(target).click();
+			$("#Pernyataan").prop("checked", true);
+			$("#mbuh").removeClass("alert alert-danger");
+			$("#mbuh").addClass("alert alert-success");
+		}
+	}
+}
