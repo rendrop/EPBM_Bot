191de2b219b37b184743504e2a6612c96f25494b
rendrop <gusvananda@gmail.com>
2019-12-06 17:47:49 +0700

    Update epbm_bot.js
    Update for 2019 changes.

--- epbm_bot.js
+++ epbm_bot.js
@@ -3,19 +3,19 @@ function hackEPBM(jumlah_dosen, kesan){
 	ma = kesan + 1
 	for(i=1;i<=10;i++){
 		choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
-		formattedNumber = ("6" + i).slice(-2)
+		formattedNumber = ("4" + i).slice(-2)
 		if(i==10){
-			formattedNumber = ("70")
+			formattedNumber = ("50")
 		}
-		target = "#JawabanMK_15"+formattedNumber+choose
+		target = "#JawabanMK_16"+formattedNumber+choose
 		$(target).click();
 	}
 	// isi form dosen
 	for(i=1;i<=jumlah_dosen;i++){
 		for(j=1;j<=10;j++){
-			formattedNumber = ("163" + j)
+			formattedNumber = ("171" + j)
 		if(j==10){
-			formattedNumber = ("1640")
+			formattedNumber = ("1720")
 }
 			choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
 			target = "#JawabanDosen_"+i+j+formattedNumber+choose
