f3ddeb8ac1dbee4544b160a1157e1a035a00c671
rendrop <gusvananda@gmail.com>
2021-04-10 14:23:29 +0700

    EPBM Bot Mid Semester Online Class

--- epbm_bot.js
+++ epbm_bot.js
@@ -1,28 +1,30 @@
 function hackEPBM(jumlah_dosen, kesan){
 	mi = kesan
 	ma = kesan + 1
+    
+	// isi form matkul
 	for(i=1;i<=10;i++){
 		choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
-		formattedNumber = ("4" + i).slice(-2)
-		if(i==10){
-			formattedNumber = ("50")
-		}
-		target = "#JawabanMK_16"+formattedNumber+choose
-		$(target).click();
+		formattedNumber = (37 + i) 
+		target = "#JawabanMK_22"+formattedNumber+choose
+	$(target).click();
 	}
+	
 	// isi form dosen
 	for(i=1;i<=jumlah_dosen;i++){
-		for(j=1;j<=10;j++){
-			formattedNumber = ("171" + j)
-		if(j==10){
-			formattedNumber = ("1720")
-}
+		for(j=1;j<=11;j++){
+			formattedNumber = ("22" + (47 + j))
 			choose = Math.floor(Math.random() * (ma - mi + 1)) + mi
 			target = "#JawabanDosen_"+i+j+formattedNumber+choose
 			$(target).click();
-			$("#Pernyataan").prop("checked", true);
-			$("#mbuh").removeClass("alert alert-danger");
-			$("#mbuh").addClass("alert alert-success");
 		}
 	}
+	$("#Pernyataan").prop("checked", true);
+	$("#mbuh").removeClass("alert alert-danger");
+	$("#mbuh").addClass("alert alert-success");
+	
+	// i = dosen ke
+	// j = pertanyaan ke
+	// formattedNumber = pattern
+	
 }
